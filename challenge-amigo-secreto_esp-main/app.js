161e16eff2b05e23d7b07938ea10a5b887c8763f
catherine <ypanaquec@gmail.com>
2025-03-13 19:32:20 -0300

    todas las funciones creadas y actualizadas

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -6,7 +6,29 @@ function agregarAmigo() {
    
     if(nombre === ""){
         alert("Por favor, inserte un nombre.");
-        return
+        
+    }else {
+   amigos.push(nombre);
+   input.value = ""; 
+   actualizarLista();
     }
-   
+}
+
+function actualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    document.getElementById('resultado').innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
 }
